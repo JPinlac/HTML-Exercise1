fe711d7434a7e234987d0f2287fdd45e5b91c65d
Jonathan <jonppinlac@gmail.com>
2015-11-17 19:30:15 -0500

    Update

--- classLabs/lab11/script.js
+++ classLabs/lab11/script.js
@@ -4,19 +4,28 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'view1.html'
+      templateUrl: 'view1.html';
+      controller: 'counterController'
     })
     .when('/view2', {
       templateUrl: 'view2.html'
+      controller: 'counterController'
     })
     .when('/view3', {
       templateUrl: 'view3.html'
+      controller: 'counterController'
     })
     .otherwise({
       redirect: '/'
+      controller: 'counterController'
     });
 });
 
-app.controller("routeCtrl", function($scope){
-  console.log("WHOA")
-})
+app.controller("counterController", function ($scope){
+    $scope.list = [];
+    $scope.total = 0;
+    $scope.add = function (item, price){
+        var itemList = {name: item, price: price}
+        $scope.total += Number(price);
+        $scope.list.push(itemList);
+    };
